1eac89f3da8708ddef03cbacf85925a48492a706
GabEstR <gestrell@montgomerycollege.edu>
2020-10-07 20:32:11 -0400

    Update scripts.js

--- Lab_4/js/scripts.js
+++ Lab_4/js/scripts.js
@@ -8,27 +8,27 @@ document.querySelector(".header").innerHTML = myFullName + "'s Lab 4\n";
 // 2 Insert two paragraphs into the div with the class "content"
 //  Label each paragraph with a distinct class name
 
-var paragraph1 = document.createElement("P1");
-var paragraph2 = document.createElement("P2");
-var p1_text = document.createTextNode("First paragraph. \n");
-var p2_text = document.createTextNode("Second paragraph. \n");
+let paragraph1 = document.createElement("P");
+let paragraph2 = document.createElement("P");
+let p1_text = document.createTextNode("First paragraph. \n");
+let p2_text = document.createTextNode("Second paragraph. \n");
 
 paragraph1.appendChild(p1_text);
 paragraph2.appendChild(p2_text);
 
-var contentSection = document.querySelector(".content");
+let contentSection = document.querySelector(".content");
 contentSection.appendChild(paragraph1);
 contentSection.appendChild(paragraph2);
 
 // 3 Into the first paragraph, insert the phrase "my name has " length of your name " characters"
 //      (e.g. my name has 10 characters).
 
-var nameLength = "My name has " + myFullName.length + " characters.";
+let nameLength = "My name has " + myFullName.length + " characters.";
 paragraph1.append(nameLength);
 
 // 4 & 5 Into the second paragraph tag, return the 3rd character in your first name
 
-var thirdCharacter = myFullName.charAt(2);
+let thirdCharacter = myFullName.charAt(2);
 
 paragraph2.append(thirdCharacter);
 
@@ -38,7 +38,7 @@ paragraph2.append("\n");
 
 // 7 Return the final three characters of your last name to that new line
 
-var finalThreeCharacters = myFullName.substring(myFullName.length - 3, myFullName.length);
+let finalThreeCharacters = myFullName.substring(myFullName.length - 3, myFullName.length);
 paragraph2.append(finalThreeCharacters);
 
 // 8 Substring your first and last name into two separate variables
