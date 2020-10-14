94497a6a779fc9a261f39ea738ac4166d10245c8
GabEstR <gestrell@montgomerycollege.edu>
2020-10-13 20:49:15 -0400

    Update scripts.js

--- Lab_5/scripts.js
+++ Lab_5/scripts.js
@@ -94,12 +94,12 @@ btn.onclick=function randomContries() {
     {
         randomNumbers=Math.floor(Math.random() * countryInitials.length);
 
-        a1=document.createElement("P1");
+        a1=document.createElement("p1");
         a2=document.createTextNode(countryName[randomNumbers] + " ");
         a1.appendChild(a2);
         a1.className="countryName";
         
-        b1=document.createElement("P2");
+        b1=document.createElement("p2");
         b2=document.createTextNode(countryInitials[randomNumbers]);
         b1.appendChild(b2);
         b1.className="countryCode";
