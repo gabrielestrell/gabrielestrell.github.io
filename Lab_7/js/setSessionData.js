9282f26414f826017c46b0fd1da24b09c0d0665b
GabEstR <gestrell@montgomerycollege.edu>
2020-11-04 23:07:04 -0500

    Update setSessionData.js

--- Lab_7/js/setSessionData.js
+++ Lab_7/js/setSessionData.js
@@ -16,15 +16,37 @@ function getBusRoute() {
         console.log("Stored bus route in session")
         console.log(route);
 
-        /*
-        let title = route.title;
-        let latMax = route.lat_max;
-        let latMin = route.lat_min;
-        let lonMax = route.lon_max;
-        let lonMin = route.lon_min;
-        */
+        
+        let title;
+        let lat_max;
+        let lat_min;
+        let lon_max;
+        let lon_min;
 
-        main.innerHTML = "Stored bus route in session" + JSON.stringify(route);
+        sessionStorage.setItem('title', route.title);
+        console.log(route);
+
+        sessionStorage.setItem('lat_max', route.lat_max);
+        console.log(route);
+
+        sessionStorage.setItem('lat_min', route.lat_min);
+        console.log(route);
+
+        sessionStorage.setItem('lon_max', route.lon_max);
+        console.log(route);
+
+        sessionStorage.setItem('lon_min', route.lon_min);
+        console.log(route);
+
+        //main.innerHTML = "Stored bus route in session" + JSON.stringify(route);
+
+        main.innerHTML = title + 
+        "<br>Lat max: " + lat_max +
+        "<br>Lat min: " + lat_min +
+        "<br>Lot max: " + lon_max +
+        "<br>Lot min: " + lon_min;
+
+
       })
       .catch((err) => {
         console.log(err);
