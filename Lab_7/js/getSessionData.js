8e17ba31897db36d13bbfdbffac3041d664f5499
GabEstR <gestrell@montgomerycollege.edu>
2020-11-06 00:08:23 -0500

    Update getSessionData.js

--- Lab_7/js/getSessionData.js
+++ Lab_7/js/getSessionData.js
@@ -1,9 +1,10 @@
 // Get saved data from sessionStorage
-let title = sessionStorage.getItem("bus_route_info"); // Your code here
-let lat_max = sessionStorage.getItem("bus_route_info");
-let lat_min = sessionStorage.getItem("bus_route_info");
-let lon_max = sessionStorage.getItem("bus_route_info");
-let lon_min = sessionStorage.getItem("bus_route_info");
+let title = sessionStorage.getItem("title", route.title); // Your code here
+let lat_max = sessionStorage.getItem("lat_max", route.lat_max);
+let lat_min = sessionStorage.getItem("lat_min", route.lat_min);
+let lon_max = sessionStorage.getItem("lon_max", route.lon_max);
+let lon_min = sessionStorage.getItem("lon_min", route.lon_min);
 
 // Your code here
 // Set the corresponding <p> elements in the GetSessionData.html page with the values from above
+document.getElementById("title").innerHTML = title;
