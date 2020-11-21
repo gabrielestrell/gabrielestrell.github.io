9851806ac96ec2e4f8c40317857ec4e7f5b54bb9
GabEstR <gestrell@montgomerycollege.edu>
2020-11-20 20:58:08 -0500

    Update lab8script.js

--- Lab_8/public/js/lab8script.js
+++ Lab_8/public/js/lab8script.js
@@ -56,7 +56,7 @@ async function getData() {
             }
         })
         .then(values => console.log(chartValues));
-        chart.render(); // Do you need to remove the comments from here in order to get it to work?
+        //chart.render(); // Do you need to remove the comments from here in order to get it to work?
 };
 
 async function makeChart() {
